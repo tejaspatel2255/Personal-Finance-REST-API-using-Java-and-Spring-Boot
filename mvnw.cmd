0811a2ece793335c5e62bed59ddb21ad7ee5c801
tejaspatel2255 <tejaspatel22505@gmail.com>
2026-08-05 10:33:52 +0530

    fix(build): enclose file paths in quotes in mvnw.cmd for directories with spaces

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,13 +1,12 @@
 @ECHO OFF
 @IF "%__JAVA_HOME%"=="" SET "JAVA_HOME=%JAVA_HOME%"
 @IF "%JAVA_HOME%"=="" SET "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.19.10-hotspot"
-set MAVEN_PROJECTBASEDIR=%~dp0
-if "%MAVEN_PROJECTBASEDIR%" == "" set MAVEN_PROJECTBASEDIR=%CD%
-set MAVEN_CMD_LINE_ARGS=%*
-set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
-if exist %WRAPPER_JAR% goto run
-echo Error: Could not find %WRAPPER_JAR%
-exit /b 1
+SET "MAVEN_PROJECTBASEDIR=%~dp0"
+IF "%MAVEN_PROJECTBASEDIR%" == "" SET "MAVEN_PROJECTBASEDIR=%CD%\"
+SET "WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar"
+SET "WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain"
+IF EXIST "%WRAPPER_JAR%" GOTO run
+ECHO Error: Could not find "%WRAPPER_JAR%"
+EXIT /b 1
 :run
-"%JAVA_HOME%\bin\java.exe" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" -classpath %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
+"%JAVA_HOME%\bin\java.exe" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR:~0,-1%" -classpath "%WRAPPER_JAR%" %WRAPPER_LAUNCHER% %*
