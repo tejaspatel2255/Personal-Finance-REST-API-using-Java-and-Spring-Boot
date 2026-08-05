26b3368340044e7dcfe559d4323306f4ecbe4c90
tejaspatel2255 <tejaspatel22505@gmail.com>
2026-08-05 10:31:47 +0530

    build: add standalone Maven Wrapper script mvnw.cmd for zero-setup execution

---
+++ mvnw.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+@IF "%__JAVA_HOME%"=="" SET "JAVA_HOME=%JAVA_HOME%"
+@IF "%JAVA_HOME%"=="" SET "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.19.10-hotspot"
+set MAVEN_PROJECTBASEDIR=%~dp0
+if "%MAVEN_PROJECTBASEDIR%" == "" set MAVEN_PROJECTBASEDIR=%CD%
+set MAVEN_CMD_LINE_ARGS=%*
+set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+if exist %WRAPPER_JAR% goto run
+echo Error: Could not find %WRAPPER_JAR%
+exit /b 1
+:run
+"%JAVA_HOME%\bin\java.exe" -classpath %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
